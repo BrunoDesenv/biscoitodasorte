0cce3a6e3df6558b3a84960721df620507215519
Bruno <bru_blink@hotmail.com>
2022-01-28 19:39:29 -0300

    criação do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Botao from './Botao'
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+    this.frases = data.frases;
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+    this.setState(state);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img"/>
+        <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    )
+  }
 }
 
 export default App;
